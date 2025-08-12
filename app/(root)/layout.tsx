b78cf5f2baf426bc1fa775370ca2fc1e2023e9d4
Priyanshu Keshri <priyanshukeshri2002@gmail.com>
2025-08-13 00:23:38 +0530

    third commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 
-import { isAuthenticated } from "@/lib/actions/auth.action";
+import { isAuthenticated, signOut } from "@/lib/actions/auth.action";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
@@ -11,11 +11,21 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <div className="root-layout">
-      <nav>
+      <nav className="flex justify-between items-center px-4 py-3 bg-slate-900">
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
           <h2 className="text-primary-100">PrepWise</h2>
         </Link>
+
+        {/* Sign Out Button */}
+        <form action={signOut}>
+          <button
+  type="submit"
+  className="bg-slate-700 text-white px-4 py-2 rounded-full font-semibold hover:bg-slate-800 transition-colors whitespace-nowrap"
+>
+  Sign Out
+</button>
+        </form>
       </nav>
 
       {children}
